1df7f42bde60a0a128372eadc2411a8712f3dfdc
Vincorp21 <vibarcar@gmail.com>
2022-06-20 00:09:40 +0200

    jugando con Estados

--- src/components/Estado.jsx
+++ src/components/Estado.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+//HIJO
+function EstadoAHijo(props) {
+  return (
+    <div>
+      <h3>{props.contadorHijo}</h3>
+    </div>
+  );
+}
+//PADRE MIN 17 IMPORTANTE Un componente hijo al ser funcional y no utilizar Hooks, los componentes funcionales no pueden tener estado. EL estado dle padre se le pasó como propiedad al componente Hijo. Cada vez que un componente recibe una propiedad, y esa propiedad modifica su estado. También va  agenerar el renderizado del pintado en pantalla.
+
 export class Estado extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +25,7 @@ export class Estado extends Component {
       <div>
         <h2>EL Estado</h2>
         <p>{this.state.contador}</p>
+        <EstadoAHijo contadorHijo={this.state.contador} />
       </div>
     );
   }
